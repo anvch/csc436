ca6b112b3cdfc550c7ce3cf6cd9080b11bae0d1e
Angela Chen <achen313@calpoly.edu>
2025-10-26 14:36:09 -0700

    7.1.2

--- csc436/PetAdoption_Starter/app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
+++ csc436/PetAdoption_Starter/app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
@@ -1,5 +1,6 @@
 package com.zybooks.petadoption.ui
 
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -29,4 +30,36 @@ fun PetAppBar(
       ),
       modifier = modifier
    )
+}
+
+@Composable
+fun ListScreen(
+   petList: List<Pet>,
+   onImageClick: (Pet) -> Unit,
+   modifier: Modifier = Modifier
+) {
+   Scaffold(
+      topBar = {
+         PetAppBar(
+            title = "Find a Friend"
+         )
+      }
+   ) { innerPadding ->
+      LazyVerticalGrid(
+         columns = GridCells.Adaptive(minSize = 128.dp),
+         contentPadding = PaddingValues(0.dp),
+         modifier = modifier.padding(innerPadding)
+      ) {
+         items(petList) { pet ->
+            Image(
+               painter = painterResource(id = pet.imageId),
+               contentDescription = "${pet.type} ${pet.gender}",
+               modifier = Modifier.clickable(
+                  onClick = { onImageClick(pet) },
+                  onClickLabel = "Select the pet"
+               )
+            )
+         }
+      }
+   }
 }
