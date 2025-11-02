93205329f3237856dd79dc2749490d6a868b3112
Angela Chen <achen313@calpoly.edu>
2025-11-02 14:46:13 -0800

    8.4.6

--- csc436/CountdownTimer/app/src/main/java/com/zybooks/countdowntimer/ui/TimerViewModel.kt
+++ csc436/CountdownTimer/app/src/main/java/com/zybooks/countdowntimer/ui/TimerViewModel.kt
@@ -63,5 +63,16 @@ class TimerViewModel : ViewModel() {
 
    fun cancelTimer() {
       // TODO: Cancel the timer
+      if (isRunning) {
+         timerJob?.cancel()
+         isRunning = false
+         remainingMillis = 0
+      }
+
+   }
+
+   override fun onCleared() {
+      super.onCleared()
+      timerJob?.cancel()
    }
 }
